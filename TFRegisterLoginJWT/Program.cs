fd61fe25b3c15c51c873da2cc26bd4b6b50655b8
a.arabuli <a.arabuli@AARABULI-LP>
2024-05-31 18:57:50 +0400

    Init

--- TFRegisterLoginJWT/Program.cs
+++ TFRegisterLoginJWT/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using TFRegisterLoginJWT.Data;
 using TFRegisterLoginJWT.Interfaces;
 using TFRegisterLoginJWT.Services;
@@ -19,10 +23,35 @@ namespace TFRegisterLoginJWT
 			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 			builder.Services.AddScoped<IAuthService, AuthService>();
+			builder.Services.AddScoped<IUserService, UserService>();
 
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
+
+			builder.Services.AddSwaggerGen(c =>
+			{
+				c.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+				{
+					Description = "Standard authorization header using the bearer scheme, e.g \"bearer {token} \"",
+					In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+					Name = "Authorization",
+					Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
+				});
+
+				c.OperationFilter<SecurityRequirementsOperationFilter>();
+			});
+
+			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+
+					options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+						.GetBytes(builder.Configuration.GetSection("Token:Secret").Value)),
+						ValidateIssuer = false,
+						ValidateAudience = false
+					});
 
 			var app = builder.Build();
 
@@ -34,6 +63,8 @@ namespace TFRegisterLoginJWT
 			}
 
 			app.UseHttpsRedirection();
+
+			app.UseAuthentication();
 
 			app.UseAuthorization();
 
---
+++ TFRegisterLoginJWT/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TFRegisterLoginJWT.Data;
+using TFRegisterLoginJWT.Data.Models;
+using TFRegisterLoginJWT.Interfaces;
+
+namespace TFRegisterLoginJWT.Services
+{
+	public class UserService : IUserService
+	{
+		private readonly ApplicationDbContext _db;
+		public UserService(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public async Task<ServiceResponse<List<User>>> GetUsers()
+		{
+			var users = await _db.Users.ToListAsync();
+			return new() { Data = users, Success = true};
+		}
+	}
+}
